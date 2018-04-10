8337cab520fd65a1139cf1e4fda2b9ac7c7d09e9
Toni Martinčić <toni.martincic@fer.hr>
2018-04-10 20:43:38 +0200

    code added in reducers

---
+++ frontend/src/reducers/userDataReducer.js
@@ -0,0 +1,8 @@
+import initialState from './initialState';
+
+export default function userDataReducer(state = initialState.userData, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
