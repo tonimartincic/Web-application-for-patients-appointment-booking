f63add6e766749734ac9809302a985a5570743a9
Toni Martinčić <toni.martincic@fer.hr>
2018-04-19 19:42:24 +0200

    medical specialists and general practitioners tables added

--- frontend/src/ui/components/users/generalPractitioners/GeneralPractitioners.js
+++ frontend/src/ui/components/users/generalPractitioners/GeneralPractitioners.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Col, Grid, Row, Table} from 'react-bootstrap';
 import NavigationBar from '../../navigationBar/NavigationBar';
 import Footer from '../../footer/Footer';
 
@@ -8,15 +9,55 @@ class GeneralPractitioners extends React.Component {
     return (
       <section>
         <NavigationBar/>
-        <span>GeneralPractitioners</span>
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <Table striped bordered condensed hover>
+                <thead>
+                <tr>
+                  <th>Ime</th>
+                  <th>Prezime</th>
+                  <th>Mail</th>
+                  <th>Grad</th>
+                  <th>Poštanski broj</th>
+                  <th>Ulica</th>
+                  <th>Kućni Broj</th>
+                  <th>Broj mobitela</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                  this.props.generalPractitioners
+                    .map((generalPractitioner, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{generalPractitioner.firstName}</td>
+                            <td>{generalPractitioner.lastName}</td>
+                            <td>{generalPractitioner.mail}</td>
+                            <td>{generalPractitioner.city}</td>
+                            <td>{generalPractitioner.postalCode}</td>
+                            <td>{generalPractitioner.street}</td>
+                            <td>{generalPractitioner.streetNumber}</td>
+                            <td>{generalPractitioner.phoneNumber}</td>
+                          </tr>)
+                      }
+                    )
+                }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
         <Footer/>
       </section>
     );
   }
 }
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state) {
+  return {
+    generalPractitioners: state.generalPractitioners,
+  };
 }
 
 function mapDispatchToProps() {
--- frontend/src/ui/components/users/medicalSpecialists/MedicalSpecialists.js
+++ frontend/src/ui/components/users/medicalSpecialists/MedicalSpecialists.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Col, Grid, Row, Table} from 'react-bootstrap';
 import NavigationBar from '../../navigationBar/NavigationBar';
 import Footer from '../../footer/Footer';
 
@@ -8,15 +9,47 @@ class MedicalSpecialists extends React.Component {
     return (
       <section>
         <NavigationBar/>
-        <span>MedicalSpecialists</span>
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <Table striped bordered condensed hover>
+                <thead>
+                <tr>
+                  <th>Ime</th>
+                  <th>Prezime</th>
+                  <th>Mail</th>
+                  <th>Broj mobitela</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                  this.props.medicalSpecialists
+                    .map((medicalSpecialist, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{medicalSpecialist.firstName}</td>
+                            <td>{medicalSpecialist.lastName}</td>
+                            <td>{medicalSpecialist.mail}</td>
+                            <td>{medicalSpecialist.phoneNumber}</td>
+                          </tr>)
+                      }
+                    )
+                }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
         <Footer/>
       </section>
     );
   }
 }
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state) {
+  return {
+    medicalSpecialists: state.medicalSpecialists,
+  };
 }
 
 function mapDispatchToProps() {
