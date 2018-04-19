86475448dec08b5751a27cf806b490cc333e5258
Toni Martinčić <toni.martincic@fer.hr>
2018-04-19 15:31:14 +0200

    administrators table added

--- frontend/src/ui/components/users/administrators/Administrators.js
+++ frontend/src/ui/components/users/administrators/Administrators.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Col, Grid, Row, Table} from 'react-bootstrap';
 import NavigationBar from '../../navigationBar/NavigationBar';
 import Footer from '../../footer/Footer';
 import styles from './administrators.css';
@@ -7,17 +8,47 @@ import styles from './administrators.css';
 class Administrators extends React.Component {
   render() {
     return (
-      <section className={styles.section}>
+      <section>
         <NavigationBar/>
-        <span>Administrators</span>
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <Table striped bordered condensed hover className={styles.table}>
+                <thead>
+                <tr>
+                  <th>Ime</th>
+                  <th>Prezime</th>
+                  <th>Mail</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                  this.props.administrators
+                    .map((administrator, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{administrator.firstName}</td>
+                            <td>{administrator.lastName}</td>
+                            <td>{administrator.mail}</td>
+                          </tr>)
+                      }
+                    )
+                }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
         <Footer/>
       </section>
     );
   }
 }
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state) {
+  return {
+    administrators: state.administrators,
+  };
 }
 
 function mapDispatchToProps() {
