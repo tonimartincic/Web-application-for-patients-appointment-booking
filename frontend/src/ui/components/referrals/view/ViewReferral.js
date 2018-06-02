d89b008fbfa79011e7773e0b40a5a9a93ba9e96b
Toni Martinčić <toni.martincic@fer.hr>
2018-06-02 18:55:01 +0200

    referral view

--- frontend/src/ui/components/referrals/view/ViewReferral.js
+++ frontend/src/ui/components/referrals/view/ViewReferral.js
@@ -12,43 +12,112 @@ class ViewReferral extends React.Component {
         >
           <Choose>
             <When condition={this.props.referralSelected}>
-              <Modal.Header closeButton>
-                <Modal.Title className={styles.modalTitle}>
-                  Pregled uputnice
-                </Modal.Title>
-              </Modal.Header>
               <Modal.Body>
                 <FormGroup controlId="formControlsSelect">
                   <ListGroup>
                     <Row>
                       <Col md={12}>
-                        <ListGroupItem>
-                          <Row>
-                            <Col md={12}>
-                              <p><b>Pacijent:</b> {this.props.referral.patient.firstName} {this.props.referral.patient.lastName}</p>
-                            </Col>
-                          </Row>
+                        <section className={styles.section}>
+                          <span className={styles.span}>PACIJENT</span>
+                        </section>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={6}>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Ime i prezime:</b> {this.props.referral.patient.firstName} {this.props.referral.patient.lastName}</p>
                         </ListGroupItem>
-                        <ListGroupItem>
-                          <Row>
-                            <Col md={12}>
-                              <p><b>Vrsta uputnice:</b> {this.props.referral.referralGroup}</p>
-                            </Col>
-                          </Row>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>OIB:</b> {this.props.referral.patient.oib}</p>
                         </ListGroupItem>
-                        <ListGroupItem>
-                          <Row>
-                            <Col md={12}>
-                              <p><b>Podvrsta uputnice:</b> {this.props.referral.referralName}</p>
-                            </Col>
-                          </Row>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Spol:</b> {this.props.referral.patient.sex}</p>
                         </ListGroupItem>
-                        <ListGroupItem>
-                          <Row>
-                            <Col md={12}>
-                              <p><b>Datum kreiranja:</b> {this.props.referral.createdOn}</p>
-                            </Col>
-                          </Row>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Datum rođenja:</b> {this.props.referral.patient.dateOfBirth}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Mail:</b> {this.props.referral.patient.mail}</p>
+                        </ListGroupItem>
+                      </Col>
+                      <Col md={6}>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Grad:</b> {this.props.referral.patient.addressData.city}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Poštanski broj:</b> {this.props.referral.patient.addressData.postalCode}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Ulica:</b> {this.props.referral.patient.addressData.street}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Kućni broj:</b> {this.props.referral.patient.addressData.streetNumber}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Telefonski broj:</b> {this.props.referral.patient.phoneNumber}</p>
+                        </ListGroupItem>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={12}>
+                        <section className={styles.section}>
+                          <span className={styles.span}>UPUTNICA ZA</span>
+                        </section>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={12}>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Vrsta uputnice:</b> {this.props.referral.referralGroup}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Podvrsta uputnice:</b> {this.props.referral.referralName}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Odjel:</b> {this.props.referral.departmentType}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Dijagnoza:</b> {this.props.referral.diagnosis}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Napomena:</b> {this.props.referral.remark}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Datum kreiranja:</b> {this.props.referral.createdOn}</p>
+                        </ListGroupItem>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={12}>
+                        <section className={styles.section}>
+                          <span className={styles.span}>SPECIJALIST OBITELJSKE MEDICINE</span>
+                        </section>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={6}>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Ime i prezime:</b> {this.props.referral.generalPractitioner.firstName} {this.props.referral.generalPractitioner.lastName}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Mail:</b> {this.props.referral.generalPractitioner.mail}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Telefonski broj:</b> {this.props.referral.generalPractitioner.phoneNumber}</p>
+                        </ListGroupItem>
+                      </Col>
+                      <Col md={6}>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Grad:</b> {this.props.referral.generalPractitioner.addressData.city}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Poštanski broj:</b> {this.props.referral.generalPractitioner.addressData.postalCode}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Ulica:</b> {this.props.referral.generalPractitioner.addressData.street}</p>
+                        </ListGroupItem>
+                        <ListGroupItem className={styles.listGroupItem}>
+                          <p><b>Kućni broj:</b> {this.props.referral.generalPractitioner.addressData.streetNumber}</p>
                         </ListGroupItem>
                       </Col>
                     </Row>
